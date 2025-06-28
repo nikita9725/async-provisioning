fe049f63c65c5c9036f2515435bda563b53251ff
nikita <nikita.grishin@hyperus.team>
2025-06-28 20:29:54 +0300

    Добавлен метод API для просмотра статусов задач

--- service_b/run.py
+++ service_b/run.py
@@ -1,6 +1,8 @@
 import uvicorn
-from fastapi import FastAPI, status
-from common.schemas import Serial, RequestModel, CreateTaskResponse
+from fastapi import FastAPI, status, HTTPException
+from starlette.responses import JSONResponse
+
+from common.schemas import Serial, RequestModel, CreateTaskResponse, ResponseModel, TaskStatus
 from common import db
 
 
@@ -30,6 +32,38 @@ async def create_task(id: Serial, payload: RequestModel) -> CreateTaskResponse:
         await session.commit()
         await session.refresh(task)
     return CreateTaskResponse(code=status.HTTP_201_CREATED, task_id=task.id)
+
+
+@app.get(
+    '/api/v1/equipment/cpe/{id}/task/{task}',
+    tags=["Tasks"],
+    summary="Получить сатус задачи для CPE",
+    response_model=ResponseModel,
+    response_description="Статус задачи",
+    responses={
+        status.HTTP_200_OK: {"model": ResponseModel, "description": "Ok"},
+        status.HTTP_404_NOT_FOUND: {"model": ResponseModel, "description": "Task not found"},
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": ResponseModel, "description": "Internal error"},
+    },
+)
+async def get_status(id: Serial, task: int) -> JSONResponse:
+    task_status_resp_map = {
+        TaskStatus.SUCCESS: (status.HTTP_200_OK, "Completed"),
+        TaskStatus.FAILED: (status.HTTP_500_INTERNAL_SERVER_ERROR, "Internal provisioning exception"),
+    }
+    async with db.AsyncSessionLocal() as session:
+        task = await session.get(db.Task, task)
+        if not task:
+            raise HTTPException(status.HTTP_404_NOT_FOUND, 'The requested task is not found')
+        if task.equipment_id != id:
+            raise HTTPException(status.HTTP_404_NOT_FOUND, 'The requested equipment is not found')
+        status_code, msg = task_status_resp_map.get(
+            task.status, (status.HTTP_200_OK, "Task is still running")
+        )
+        return JSONResponse(
+            status_code=status_code,
+            content=ResponseModel(code=status_code, message=msg).model_dump(),
+        )
 
 
 def run_service(port: int = 8001) -> None:
